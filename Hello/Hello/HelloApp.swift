b4fee4bb07d5d3305e10607b1a6dca1a365421ce
Fishzz <fishzz@Fishzzs-MacBook-Pro.local>
2021-04-06 13:04:50 -0700

    working

---
+++ Hello/Hello/HelloApp.swift
@@ -0,0 +1,17 @@
+//
+//  HelloApp.swift
+//  Hello
+//
+//  Created by Fishzz on 4/6/21.
+//
+
+import SwiftUI
+
+@main
+struct HelloApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
